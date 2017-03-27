f444c68b030fb05f7395090dc5483333bcd52708
Darcy Parker <darcyparker@gmail.com>
2017-03-27 17:34:56 -0400

    Fix minor tslint style issue

--- test.ts
+++ test.ts
@@ -4,8 +4,8 @@ console.assert(typeof WKT !== undefined);
 
 // convert a GeoJSON Geometry to a WKT Geometry
 let wktPoint = WKT.convert({
-  "type": "Point",
-  "coordinates": [-122.6764, 45.5165]
+  type: "Point",
+  coordinates: [-122.6764, 45.5165]
 });
 
 // convert a GeoJSON object into an ArcGIS geometry
